89c336c232a2c06b39b5b9726186c61d56aabc57
leanhdudu <leanhdudu@email.com>
2017-08-18 10:14:27 +0700

    Fri 08/18/2017 10:14:22.51

---
+++ js/custom.js
@@ -0,0 +1,34 @@
+$(function () {
+    $.fn.limit = function ($n) {
+        this.each(function () {
+            var allText = $(this).html();
+            var tLength = $(this).html().length;
+            var startText = allText.slice(0, $n);
+            if (tLength >= $n) {
+                $(this).html(startText + '...');
+            } else {
+                $(this).html(startText);
+            };
+        });
+
+        return this;
+    }
+    $('.excerpt p').limit(200);
+});
+$(function () {
+    $.fn.limit = function ($n) {
+        this.each(function () {
+            var allText = $(this).html();
+            var tLength = $(this).html().length;
+            var startText = allText.slice(0, $n);
+            if (tLength >= $n) {
+                $(this).html(startText + '...');
+            } else {
+                $(this).html(startText);
+            };
+        });
+
+        return this;
+    }
+    $('.excerpt .small').limit(95);
+});
